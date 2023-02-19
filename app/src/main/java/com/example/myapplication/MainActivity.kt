485d8adc9421546c5a0525b535a95f9d77de47e5
Kai Delay <kbde@riseup.net>
2023-02-19 11:41:25 +0100

    baseadapter prototype

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +18,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val photoAdapter = PhotoAdapter(this, DataManager.photos.toList())
+        binding.photoDisplay.adapter = photoAdapter
     }
+}
+
+class PhotoAdapter(val _context : Context, var photos : List<photo>) : BaseAdapter() {
+
+    val resources = arrayOf(R.drawable.one,
+        R.drawable.two, R.drawable.three,
+        R.drawable.four,
+        R.drawable.five,
+        R.drawable.six,
+    )
+    override fun getCount(): Int {
+        return photos.count()
+    }
+
+    override fun getItem(p0: Int): Any {
+        return photos[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        val photo = photos[p0]
+        val resource = resources[p0]
+
+        var image = ImageView(_context)
+        image.setImageResource(resource)
+
+        return image
+    }
+
 }
